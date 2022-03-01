077a35d51e2f374660203439ec3bda1909b64292
joelmeiller <joel@oscillate.ch>
2022-03-01 11:11:36 +0100

    upgrade to version Meteor version 2.3

--- package.js
+++ package.js
@@ -1,22 +1,22 @@
 Package.describe({
   name: 'optune:auth0-oauth',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'OAuth handler for Auth0',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.3');
+  api.versionsFrom('2.3');
   api.use('ecmascript');
   api.use('oauth2', ['client', 'server']);
   api.use('oauth', ['client', 'server']);
-  api.use('http', ['server']);
+  api.use('fetch', ['server']);
   api.use(['underscore', 'service-configuration'], ['client', 'server']);
   api.use(['random'], 'client');
   api.use('accounts-oauth', ['client', 'server']);
-  api.use('accounts-base', ['client', 'server']);
+  api.use("accounts-base", ["client", "server"]);
   // Export Accounts (etc) to packages using this one.
-  api.imply('accounts-base', ['client', 'server']);
+  api.imply("accounts-base", ["client", "server"]);
 
   api.export('Auth0');
 
